cd054823a0b9be84c8aabb41d17107bd864b7553
Carlosqchao <carlosruanoramos@gmail.com>
2024-09-29 19:51:10 +0100

    Mi segundo Commit

--- src/software/ulpgc/Main.java
+++ src/software/ulpgc/Main.java
@@ -1,4 +1,10 @@
 package software.ulpgc;
 
+import java.time.LocalDate;
+
 public class Main {
+    public static void main(String[] args) {
+        Person person = new Person("Juan", LocalDate.of(2020,10,10));
+        System.out.println(person);
+    }
 }
